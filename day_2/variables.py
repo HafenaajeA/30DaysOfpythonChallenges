87d633420ed708218b484f4bdb67a9361c65e8ef
HafenaajeA <almandocode@gmail.com>
2025-03-30 16:35:43 +0200

    day_2 python challenges

---
+++ day_2/variables.py
@@ -0,0 +1,43 @@
+# Day 2 : 30 of python programming
+#Exercise level 1
+
+first_name = "Almando"
+last_name = "Hafenaaje"
+full_name = first_name  + " " + last_name
+country = "Namibia"
+city = "Windhoek"
+age = 250
+year = 2025
+is_married = False
+is_true = is_married
+is_light= "NO"
+
+singing, walking, moving, running = "lovisa", "petrus", "landcruser", "Kristine mboma"
+
+#print(singing, walking, moving, running)
+
+#Exercise level 2
+
+print(type(full_name))
+print(len(first_name))
+print(len(first_name) == len(last_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_div = num_one // num_two
+radius = 30
+area_of_circle = 3.14 * radius ** 2
+
+circum_of_circle = 2 * 3.14 * radius
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14 * radius ** 2
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+country = input("Enter your country: ")
+age = input("Enter your age: ")
