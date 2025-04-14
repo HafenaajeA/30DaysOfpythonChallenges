6a8bf9e35e14ae2d92243fe1d136a56e9949caa3
HafenaajeA <almandocode@gmail.com>
2025-04-14 20:16:22 +0200

    day 17 of 30 days of python challenges

---
+++ day_17/exception_handling.py
@@ -0,0 +1,7 @@
+# Day 17 of 30 days of python challenges
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+# Unpack
+first_five_countries, es, ru = names[:5], names[5], names[6]
+print(es)
